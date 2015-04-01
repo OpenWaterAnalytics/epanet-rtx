253379b9a400a89414d9f0f95a3e2babc2cfc0ba
Sam Hatchett <samhatchett@gmail.com>
2015-04-01 10:18:56 -0400

    expanded functionality for time zones

--- src/PointRecordTime.cpp
+++ src/PointRecordTime.cpp
@@ -3,13 +3,10 @@
 
 using namespace RTX;
 using namespace std;
+using namespace boost::local_time;
+using boost::posix_time::ptime;
 
-
-time_t PointRecordTime::time(SQL_TIMESTAMP_STRUCT sqlTime) {
-  
-  boost::posix_time::ptime pTimeEpoch = boost::posix_time::ptime(boost::gregorian::date(1970,1,1));
-  
-  time_t uTime;
+struct tm PointRecordTime::tmFromSql(SQL_TIMESTAMP_STRUCT sqlTime) {
   struct tm tmTime;
   
   tmTime.tm_year = sqlTime.year - 1900;
@@ -18,6 +15,17 @@ time_t PointRecordTime::time(SQL_TIMESTAMP_STRUCT sqlTime) {
   tmTime.tm_hour = sqlTime.hour;
   tmTime.tm_min = sqlTime.minute;
   tmTime.tm_sec = sqlTime.second;
+  
+  return tmTime;
+}
+
+
+time_t PointRecordTime::time(SQL_TIMESTAMP_STRUCT sqlTime) {
+  
+  ptime pTimeEpoch = boost::posix_time::ptime(boost::gregorian::date(1970,1,1));
+  
+  time_t uTime;
+  struct tm tmTime = PointRecordTime::tmFromSql(sqlTime);
   
   // Portability note: mktime is essentially universally available. timegm is rather rare. fortunately, boost has some capabilities here.
   // uTime = timegm(&tmTime);
@@ -40,6 +48,42 @@ time_t PointRecordTime::time(SQL_TIMESTAMP_STRUCT sqlTime) {
   }
   
   return uTime;
+}
+
+time_t PointRecordTime::timeFromZone(SQL_TIMESTAMP_STRUCT sqlTime, const time_zone_ptr& localtz) {
+  
+  time_t uTime;
+  // use a specified-timezone time input, and output unix epoch UTC
+  struct tm tmTime = PointRecordTime::tmFromSql(sqlTime);
+  ptime t = boost::posix_time::ptime_from_tm(tmTime);
+  ptime pTimeEpoch = boost::posix_time::ptime(boost::gregorian::date(1970,1,1));
+  
+  if(t.is_not_a_date_time()) return 0;
+  local_date_time lt(t.date(), t.time_of_day(), localtz, local_date_time::NOT_DATE_TIME_ON_ERROR);
+  ptime unixTime = lt.utc_time();
+  
+  boost::posix_time::time_duration::sec_type x = (unixTime - pTimeEpoch).total_seconds();
+  uTime = time_t(x);
+  return uTime;
+}
+
+string PointRecordTime::localDateStringFromUnix(time_t unixTime, const boost::local_time::time_zone_ptr& localtz) {
+  
+  boost::posix_time::ptime pt = boost::posix_time::from_time_t(unixTime);
+  
+  local_date_time localTime(pt,localtz);
+  
+  string s;
+  ostringstream datetime_ss;
+  boost::posix_time::time_facet * p_time_output = new boost::posix_time::time_facet;
+  // locale takes ownership of the p_time_output facet
+  datetime_ss.imbue (locale(datetime_ss.getloc(), p_time_output));
+  (*p_time_output).format("%Y-%m-%d %H:%M:%S"); // date time
+  datetime_ss << localTime.local_time();
+  s = datetime_ss.str().c_str();
+  // don't explicitly delete p_time_output
+  return s;
+  
 }
 
 
--- src/PointRecordTime.h
+++ src/PointRecordTime.h
@@ -13,13 +13,21 @@
 #include <time.h>
 #include <sqltypes.h>
 
+#ifdef check
+#undef check
+#endif  // this is to prevent mac debugger core from conflicting with boost macros
+#include <boost/date_time/local_time/local_time.hpp>
+
 namespace RTX {
   class PointRecordTime {
     
   public:
-    typedef enum { LOCAL, UTC } time_format_t;
+    typedef enum { UTC = 0, LOCAL = 1 } time_format_t;
+    static tm tmFromSql(SQL_TIMESTAMP_STRUCT sqlTime);
     static time_t time(SQL_TIMESTAMP_STRUCT sqlTime);
+    static time_t timeFromZone(SQL_TIMESTAMP_STRUCT sqlTime, const boost::local_time::time_zone_ptr& localtz);
     static SQL_TIMESTAMP_STRUCT sqlTime(time_t uTime, time_format_t format = UTC);
+    static std::string localDateStringFromUnix(time_t unixTime, const boost::local_time::time_zone_ptr& localtz);
     static std::string utcDateStringFromUnix(time_t unixTime);
   };
 }
