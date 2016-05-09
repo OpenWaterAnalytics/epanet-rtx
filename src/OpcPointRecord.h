a6d90c49b6bb6ce2bcd629703031ecb1e44694c5
Sam Hatchett <samhatchett@gmail.com>
2016-05-09 12:20:36 -0400

    adds OPC record type

---
+++ src/OpcPointRecord.h
@@ -0,0 +1,46 @@
+#ifndef OpcPointRecord_hpp
+#define OpcPointRecord_hpp
+
+#include <stdio.h>
+
+#include <opc/ua/client/client.h>
+
+#include "DbPointRecord.h"
+
+namespace RTX {
+  class OpcPointRecord : public DbPointRecord {
+  public:
+    RTX_BASE_PROPS(OpcPointRecord);
+    OpcPointRecord();
+    
+    std::string connectionString();
+    void setConnectionString(const std::string& string);
+    
+    void dbConnect() throw(RtxException);
+    bool isConnected();
+    bool supportsUnitsColumn() {return false;};
+    const std::map<std::string, Units> identifiersAndUnits();
+    
+    // stubs
+    void truncate() {};
+    bool supportsSinglyBoundedQueries() {return false;};
+    bool shouldSearchIteratively() {return false;};
+    std::vector<Point> selectRange(const std::string& id, time_t startTime, time_t endTime);
+    Point selectNext(const std::string& id, time_t time) {};
+    Point selectPrevious(const std::string& id, time_t time) {};
+    
+    // insertions or alterations: may choose to ignore / deny
+    void insertSingle(const std::string& id, Point point) {};
+    void insertRange(const std::string& id, std::vector<Point> points) {};
+    void removeRecord(const std::string& id) {};
+    bool insertIdentifierAndUnits(const std::string& id, Units units) {};
+    
+  private:
+    std::string _endpoint;
+    OpcUa::UaClient _client;
+    bool _connected;
+    std::map<string,OpcUa::Node> _nodes;
+  };
+}
+
+#endif /* OpcPointRecord_hpp */
