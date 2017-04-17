5dc44d21c7aa9bbfef3983c726eedbdc0ba28db5
Sam Hatchett <samhatchett@gmail.com>
2017-04-17 16:50:14 -0400

    disable tests

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,3 +1,11 @@
 #define __AS_MAIN
   #include "test_main.h"
 #undef __AS_MAIN
+
+#include <iostream>
+
+boost::unit_test::test_suite* init_unit_test_suite(int /*argc*/, char* /*argv*/[])
+{
+  std::cout << "using obsolete init" << std::endl;
+  return 0;
+}
