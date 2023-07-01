28e75ea12b5ea896ef1a431e10aa64965cb912a2
0tkl <118708188+0tkl@users.noreply.github.com>
2023-07-01 11:08:26 +0800

    fix(examples): clean up timeseries demo

--- examples/conceptual/timeseries_demo.cpp
+++ examples/conceptual/timeseries_demo.cpp
@@ -9,34 +9,28 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include <boost/random.hpp>
+//#include <boost/random.hpp>
 
 #include "Point.h"
 #include "TimeSeries.h"
-#include "Resampler.h"
+//#include "Resampler.h"
 #include "MovingAverage.h"
 #include "FirstDerivative.h"
 #include "SineTimeSeries.h"
 #include "ConstantTimeSeries.h"
 #include "AggregatorTimeSeries.h"
-#include "CsvPointRecord.h"
+//#include "CsvPointRecord.h"
 #include "GainTimeSeries.h"
 #include "FailoverTimeSeries.h"
-#include "TimeOffsetTimeSeries.h"
+#include "LagTimeSeries.h"
 //#include "WarpingTimeSeries.h"
 //#include "ForecastTimeSeries.h"
-#include "TimeOffsetTimeSeries.h"
 
 #include "OutlierExclusionTimeSeries.h"
 
 #include "BufferPointRecord.h"
-#include "MysqlPointRecord.h"
-
-//#include "SqliteProjectFile.h"
 
 #include "ConcreteDbRecords.h"
-
-#include "PythonInterpreter.h"
 
 using namespace RTX;
 using namespace std;
@@ -52,36 +46,26 @@ int main(int argc, const char * argv[])
   sineWave->setName("sine");
   sineWave->setClock(clock);
   
-  TimeOffsetTimeSeries::_sp lag(new TimeOffsetTimeSeries());
+  LagTimeSeries::_sp lag(new LagTimeSeries());
   lag->setOffset(900);
   lag->setSource(sineWave);
   
   time_t t1 = 1400004000; //time(NULL);
   time_t t2 = t1 + 3600;
-  vector<Point> points = lag->points(t1, t2);
+  vector<Point> points = lag->points(TimeRange(t1, t2));
   
-  printPoints(sineWave->points(t1, t2));
+  printPoints(sineWave->points(TimeRange(t1, t2)));
   cout << endl;
   printPoints(points);
   
-  
-  
   /*
-  
-  
   ForecastTimeSeries::_sp pythonTestTs(new ForecastTimeSeries());
-  
   pythonTestTs->setSource(sineWave);
-  
   vector<Point> testPoints = pythonTestTs->points(1000, 3000);
-  
   printPoints(testPoints);
   
-  
   vector<Point> range;
-  
   */
-  
   
   return 0;
 }
